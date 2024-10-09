96cb890689b713f016496dbc0c0d4fb86d8237d1
Sri keerthana G <131778969+SrikeerthanaG@users.noreply.github.com>
2024-10-09 23:51:42 +0530

    Add files via upload

---
+++ Sri keerthana/C# Assignment-Electronic Gadgets/TechShop.Exceptions/TechShop.Exceptions/ConcurrencyControl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechShop.entity;
+using TechShop.Businesslayer;
+
+namespace TechShop.Exceptions
+{
+    public class ConcurrencyException : System.Exception
+    {
+        public ConcurrencyException(string message) : base(message)
+        {
+        }
+    }
+    public class ConcurrencyService
+    {
+        private object lockObject = new object();
+
+        public void UpdateOrder(Order order)
+        {
+            lock (lockObject)
+            {
+                bool updateSuccess = false; 
+
+                if (!updateSuccess)
+                {
+                    throw new ConcurrencyException("The order was updated by another user. Please try again.");
+                }
+
+            }
+        }
+    }
+}
---
+++ Sri keerthana/C# Assignment-Electronic Gadgets/TechShop.Exceptions/TechShop.Exceptions/DataValidation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechShop.entity;
+using TechShop.Businesslayer;
+
+namespace TechShop.Exceptions  
+{
+    public class InvalidDataException : System.Exception
+    {
+        public InvalidDataException(string message) : base(message)
+        {
+        }
+    }
+
+    public class CustomerService
+    {
+        public void RegisterCustomer(string email)
+        {
+            if (!IsValidEmail(email))
+            {
+                throw new InvalidDataException("The provided email address is invalid.");
+            }
+
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            return email.Contains("@");
+        }
+    }
+}
