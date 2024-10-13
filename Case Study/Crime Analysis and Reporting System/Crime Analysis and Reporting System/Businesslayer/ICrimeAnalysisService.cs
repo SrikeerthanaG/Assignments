ba888a7bc022768614141f5a40c8d7dc91279aef
Sri keerthana G <131778969+SrikeerthanaG@users.noreply.github.com>
2024-10-13 22:45:48 +0530

    Add files via upload

---
+++ Case Study/Crime Analysis and Reporting System/Crime Analysis and Reporting System/Businesslayer/ICrimeAnalysisService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Crime_Analysis_and_Reporting_System.Entity;
+
+namespace Crime_Analysis_and_Reporting_System.Businesslayer
+{
+    public interface ICrimeAnalysisService
+    {
+        // Create a new incident
+        bool CreateIncident(Incident incident);
+
+        // Update the status of an incident
+        bool UpdateIncidentStatus(string status, int incidentID);
+
+        // Get a list of incidents within a date range
+        List<Incident> GetIncidentsInDateRange(DateTime startDate, DateTime endDate);
+
+        // Search for incidents based on various criteria
+        List<Incident> SearchIncidents(string incidentType);
+
+        // Generate incident reports
+        Report GenerateIncidentReport(Incident incident);
+
+        // Create a new victim
+        bool CreateVictim(Victim victim);
+
+        // Create a new suspect
+        bool CreateSuspect(Suspect suspect);
+
+        // Other entity-related methods (Law Enforcement Agencies, Officer, Evidence, etc.)
+        bool CreateOfficer(Officer officer);
+        bool CreateLawEnforcementAgency(LawEnforcementAgency agency);
+        bool CreateEvidence(Evidence evidence);
+        bool CreateReport(Report report);
+    }
+}
---
+++ Case Study/Crime Analysis and Reporting System/Crime Analysis and Reporting System/Util/DBUtil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crime_Analysis_and_Reporting_System.Util
+{
+        public static class DBUtil
+        {
+
+            public static SqlConnection getDBConnection()
+            {
+                SqlConnection conn;
+                string connectionstring = "Data Source=SRIKEERTHANA;Initial Catalog=CARS;Integrated Security=True;TrustServerCertificate=True";
+                conn = new SqlConnection();
+                conn.ConnectionString = connectionstring;
+                return conn;
+            }
+        }
+
+    }
+
+
+
