f32b4e38744611fd8a8ff57137c14b343a64bcae
Sri keerthana G <131778969+SrikeerthanaG@users.noreply.github.com>
2024-10-13 08:20:44 +0530

    Add files via upload

---
+++ Sri keerthana/C# Assignment-Electronic Gadgets/TechShop.Businesslayer/TechShop.Businesslayer/Repository/CustomerRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechShop.Businesslayer.Service;
+using TechShop.entity;
+
+namespace TechShop.Businesslayer.Repository
+{
+    public class CustomerRepository : ICustomerService
+    {
+        private List<Customer> customers = new List<Customer>();
+
+        public void AddCustomer(Customer customer)
+        {
+            customers.Add(customer);
+            Console.WriteLine("Customer added successfully.");
+        }
+
+        public void UpdateCustomer(Customer customer)
+        {
+            var existingCustomer = GetCustomer(customer.CustomerID);
+            existingCustomer.Email = customer.Email;
+            existingCustomer.Phone = customer.Phone;
+            existingCustomer.Address = customer.Address;
+            Console.WriteLine("Customer updated successfully.");
+        }
+
+        public Customer GetCustomer(int id)
+        {
+            return customers.FirstOrDefault(c => c.CustomerID == id) ?? throw new Exception("Customer not found.");
+        }
+
+        public int CalculateTotalOrders(int customerId)
+        {
+            return new Random().Next(1, 10);
+        }
+    }
+
+}
---
+++ Sri keerthana/C# Assignment-Electronic Gadgets/TechShop.Businesslayer/TechShop.Businesslayer/Service/IOrderDetailService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechShop.entity;
+
+namespace TechShop.Businesslayer.Service
+{
+    public interface IOrderDetailService
+    {
+        void AddOrderDetail(OrderDetail orderDetail);
+        OrderDetail GetOrderDetail(int orderDetailId);
+        void UpdateQuantity(int orderDetailId, int quantity);
+        decimal CalculateSubtotal(int orderDetailId);
+    }
+
+}
