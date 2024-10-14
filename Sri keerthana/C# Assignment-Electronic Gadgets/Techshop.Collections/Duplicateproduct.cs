dd3e8faa4e8bbf686f6c062b2e6572e81736cc57
Sri keerthana G <131778969+SrikeerthanaG@users.noreply.github.com>
2024-10-14 21:44:27 +0530

    Add files via upload

---
+++ Sri keerthana/C# Assignment-Electronic Gadgets/Techshop.Collections/Duplicateproduct.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TechShop.entity;
+using TechShop.entity;
+using TechShop.Exceptions;
+
+namespace TechShop.Collections
+{
+    public class ProductCollection
+    {
+        public List<Product> Products { get; set; } = new List<Product>();
+
+        public void AddProduct(Product product)
+        {
+            if (Products.Any(p => p.ProductName == product.ProductName))
+                throw new InvalidDataException("Duplicate product.");
+            Products.Add(product);
+        }
+
+        public void UpdateProduct(int productId, string description, decimal price)
+        {
+            var product = GetProductById(productId);
+            if (product != null) { product.Description = description; product.Price = price; }
+            else throw new InvalidDataException("Product not found.");
+        }
+
+        public void RemoveProduct(int productId)
+        {
+            var product = GetProductById(productId);
+            if (product != null) Products.Remove(product);
+            else throw new InvalidDataException("Product not found.");
+        }
+
+        public Product GetProductById(int productId) => Products.FirstOrDefault(p => p.ProductID == productId);
+
+        public List<Product> SearchProductsByName(string name) => Products.Where(p => p.ProductName.Contains(name, StringComparison.OrdinalIgnoreCase)).ToList();
+    }
+}
