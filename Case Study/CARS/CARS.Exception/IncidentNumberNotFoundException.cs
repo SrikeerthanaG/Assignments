4205eb6faabc95474e1517f630cdba9a4684fa07
Sri keerthana G <131778969+SrikeerthanaG@users.noreply.github.com>
2024-10-14 21:50:38 +0530

    Add files via upload

---
+++ Case Study/CARS/CARS.Exception/IncidentNumberNotFoundException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CARS.Exception
+{
+    public class IncidentNumberNotFoundException : System.Exception
+    {
+        public IncidentNumberNotFoundException() : base("Incident number not found.")
+        {
+        }
+
+        public IncidentNumberNotFoundException(string message) : base(message)
+        {
+        }
+
+        public IncidentNumberNotFoundException(string message, System.Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
