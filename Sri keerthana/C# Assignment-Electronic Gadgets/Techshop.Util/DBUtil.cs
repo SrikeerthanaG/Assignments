5dd2806c393623da5a9d8a0028bcce2939156b2e
Sri keerthana G <131778969+SrikeerthanaG@users.noreply.github.com>
2024-10-14 21:45:24 +0530

    Add files via upload

---
+++ Sri keerthana/C# Assignment-Electronic Gadgets/Techshop.Util/DBUtil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Techshop.Util
+{
+    public static class DBUtil
+    {
+
+        public static SqlConnection getDBConnection()
+        {
+            SqlConnection conn;
+            string connectionstring = "Data Source=SRIKEERTHANA;Initial Catalog=CARS;Integrated Security=True;TrustServerCertificate=True";
+            conn = new SqlConnection();
+            conn.ConnectionString = connectionstring;
+            return conn;
+        }
+    }
+
+}
+
+
---
+++ Sri keerthana/C# Assignment-Electronic Gadgets/Techshop.Util/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Techshop.Util")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Techshop.Util")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("39285498-26c6-4164-84c2-2cbb2e2c77ed")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
